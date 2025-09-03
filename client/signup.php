0a965d9d328aca546fb9dac890bda1cbd42afb45
harshalkotkar156 <harshalkotkar156@gmail.com>
2025-09-04 02:40:08 +0530

    first commit

---
+++ client/signup.php
@@ -0,0 +1,31 @@
+<div class="container">
+    
+    <h2>Sign-up</h2>
+<form>
+  <div class="sm-3 col-6 offset-sm-3 margin-bottom-15">
+    <label for="username" class="form-label">Username</label>
+    <input type="text" class="form-control" id="username" placeholder="enter username...">
+
+  </div>
+  <div class="sm-3 col-6 offset-sm-3 margin-bottom-15">
+    <label for="emailid" class="form-label">Email address</label>
+    <input type="email" class="form-control" id="emailid" placeholder="enter emailid...">
+
+  </div>
+
+  <div class="sm-3 col-6 offset-sm-3 margin-bottom-15">
+    <label for="password" class="form-label">Password</label>
+    <input type="password" class="form-control" id="password" placeholder="enter password...">
+
+  </div>
+
+  <div class="sm-3 col-6 offset-sm-3 margin-bottom-15">
+    <label for="address" class="form-label">Password</label>
+    <input type="text" class="form-control" id="address" placeholder="enter address...">
+
+  </div>
+  
+  <button type="submit" class="margin-bottom-15 col-6 sm-3 offset-sm-3 btn btn-primary">Submit</button>
+</form>
+
+</div>
---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Discuss</title>
+
+    <?php include("./client/commonfiles.php") ?>
+</head>
+<body>
+    <?php include("./client/header.php");
+        include("./client/signup.php");
+    ?>
+
+    
+</body>
+</html>
